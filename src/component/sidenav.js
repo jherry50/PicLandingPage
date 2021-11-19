936399b3041e7231b1ab97dc8cfe621a6f72b100
Bolarinwa Ilesanmi <bhola050@gmail.com>
2021-11-19 01:00:07 +0100

    .

--- src/component/sidenav.js
+++ src/component/sidenav.js
@@ -1,5 +1,15 @@
 import React from "react";
+import {NavLink} from 'react-router-dom';
 import logo from "../assets/blucube.PNG";
+
+import HomeIcon from '@mui/icons-material/Home';
+import MessageIcon from '@mui/icons-material/Message';
+import ElevenMpIcon from '@mui/icons-material/ElevenMp';
+import BubbleChartIcon from '@mui/icons-material/BubbleChart';
+import GroupWorkIcon from '@mui/icons-material/GroupWork';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import ParaglidingIcon from '@mui/icons-material/Paragliding';
+import GroupIcon from '@mui/icons-material/Group';
 
 const SideNav = (props) => {
     return(
@@ -7,23 +17,64 @@ const SideNav = (props) => {
            <div>
                <img src={logo} alt="logo"/>
            </div>
-           {/* <div className="menuList">
-               <ul>
-                   <li>Home</li>
-                   <li>Message</li>
+           <div className="menuList">
+               <ul className="sideMenuList">
+                   <li>
+                       <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <HomeIcon className="navItemIcon"/>
+                            Home
+                       </NavLink>
+                   </li>
+                   <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <MessageIcon className="navItemIcon"/>
+                           Message
+                       </NavLink>
+
+                   </li>
                </ul>
-               <h6>
+               <h6 className="navSubhead">
                    SHARE
                </h6>
-               <ul>
-                    <li>Ranking</li>
-                    <li>Challenge</li>
-                    <li>Party</li>
-                    <li>Connect</li>
-                    <li>Parade</li>
-                    <li>Group</li>
+               <ul className="sideMenuList">
+                    <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <ElevenMpIcon className="navItemIcon"/>
+                           Ranking
+                       </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <BubbleChartIcon className="navItemIcon"/>
+                           Challenge
+                       </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <GroupWorkIcon className="navItemIcon"/>
+                           Party
+                       </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <ConnectWithoutContactIcon className="navItemIcon"/>
+                           Connect
+                       </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <ParaglidingIcon className="navItemIcon"/>
+                           Parade
+                       </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/" className="navItem" activeClassName="activeNavItem">
+                           <GroupIcon className="navItemIcon"/>
+                           Group
+                       </NavLink>
+                    </li>
                </ul>
-           </div> */}
+           </div>
         </div>
     )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -10,7 +10,7 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import RoomIcon from '@mui/icons-material/Room';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-
+import FavoriteIcon from '@mui/icons-material/Favorite';
 //notification badge
 import Badge from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
@@ -71,15 +71,11 @@ const useStyles = makeStyles(theme => ({
     },
     searchInput:{
         boxShadow: '0px 4px 30px rgba(23, 28, 33, 0.1)',
-        // borderRadius: '50px',
         border: 'none',
         height: '62px',
         padding: '14px !important'
     },
-    // dropDown: {
-    //     width: "250px",
-    //     height: "300px",
-    // },
+ 
     dropDownToggle:{
         display: "inline-block"
     }
@@ -104,11 +100,10 @@ const Dashboard =()=>{
 
    
 
-    //This is an effect to fetch the news api on page load
+    //This is an effect to fetch the photos api on page load
     useEffect(()=>{
         axios.get(`https://api.unsplash.com/photos?page=10&client_id=W6FTtcObb327UiNFP76QTgeucULryadqXBbyUOfKvw0`)
             .then(function (response) {
-                debugger
                 // handle success
                 setData(response.data);
                 setLoader(false);
@@ -128,25 +123,24 @@ const Dashboard =()=>{
     },[]);
 
     const searchPhotos = () => {
-        debugger
-            axios.get(`https://api.unsplash.com/search/photos?page=1&query=${searchQuery}&client_id=W6FTtcObb327UiNFP76QTgeucULryadqXBbyUOfKvw0`)
-                .then(function (response) {
-                    debugger
-                    // handle success
-                    setData(response.data.results);
-                    setLoader(false);
-                
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                    alert('Error occured in loading All Data');
-                    setLoader(false);
-                })
-                .then(function () {
-                    setLoader(false);
-                    // always executed
-                });
+        axios.get(`https://api.unsplash.com/search/photos?page=1&query=${searchQuery}&client_id=W6FTtcObb327UiNFP76QTgeucULryadqXBbyUOfKvw0`)
+            .then(function (response) {
+                debugger
+                // handle success
+                setData(response.data.results);
+                setLoader(false);
+            
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+                alert('Error occured in loading All Data');
+                setLoader(false);
+            })
+            .then(function () {
+                setLoader(false);
+                // always executed
+            });
         
     }
   
@@ -167,7 +161,7 @@ const Dashboard =()=>{
                             </Grid>
                             <Grid item xs={12} md={10} className="bodyContainer">
                                 <Grid container spacing={2}>
-                                    <Grid item xs={12} md={7} lg={9}>
+                                    <Grid className="desktopSearch" item xs={12} md={7} lg={9}>
                                         <FormControl sx={{width: '100%' }} variant="outlined">
                                             <TextField
                                                 id="search-area"
@@ -213,28 +207,84 @@ const Dashboard =()=>{
                                             </DropdownToggle>
 
                                             <DropdownMenu right>
-                                                    <div>
-                                                        <p>to be listed</p>
-                                                        <p>to be listed</p>
-                                                    </div>
+                                                    <Grid container>
+                                                        <Grid item xs={3}>
+                                                            <Avatar
+                                                                alt="Bola Ilesanmi"
+                                                                src={profilePicture}
+                                                                sx={{ width: 36, height: 36 }}
+                                                            />
+                                                        </Grid>
+                                                        <Grid item xs={6}>
+                                                            <Typography  variant="body2" gutterBottom component="div">
+                                                                Michael liked you!
+                                                            </Typography>
+                                                            <Typography sx={{fontSize: '9px'}} variant="caption" gutterBottom component="div">
+                                                                About 20 minutes ago!
+                                                            </Typography>
+                                                        </Grid>
+                                                        <Grid item xs={3}>
+                                                            <div style={{width: "100%", textAlign: "center"}}>
+                                                                <FavoriteIcon fontSize="medium" style={{color: "#858390"}}/>
+                                                            </div>
+                                                        </Grid>
+                                                    </Grid>
                                             </DropdownMenu>
                                         </Dropdown>
                                     </Grid>
                                     <Grid item xs={6} md={3} lg={2}>
-                                        <StyledProfile badgeContent={"B"} >
-                                            <Avatar
-                                                alt="Bola Ilesanmi"
-                                                src={profilePicture}
-                                                sx={{ width: 56, height: 56 }}
-                                            />
-                                        </StyledProfile>
-                                        <Typography sx={{ marginLeft: '15px'}} variant="subtitle2" gutterBottom component="span">
-                                            Abigail
-                                            <IconButton>
-                                                <KeyboardArrowDownIcon/>
-                                            </IconButton>
-                                        </Typography>
+                                        <div className="userProfile">
+                                            <StyledProfile badgeContent={"B"} >
+                                                <Avatar
+                                                    alt="Bola Ilesanmi"
+                                                    src={profilePicture}
+                                                    sx={{ width: 56, height: 56 }}
+                                                />
+                                            </StyledProfile>
+                                            <Typography sx={{ marginLeft: '15px'}} variant="subtitle2" gutterBottom component="span">
+                                                Abigail
+                                                <IconButton>
+                                                    <KeyboardArrowDownIcon/>
+                                                </IconButton>
+                                            </Typography>
+                                        </div>
                                     </Grid>
+                                    <Grid className="mobileSearch" item xs={12} md={7} lg={9}>
+                                        <FormControl sx={{width: '100%' }} variant="outlined">
+                                            <TextField
+                                                id="search-area"
+                                                type='text'
+                                                variant="standard"
+                                                value={searchQuery}
+                                                onChange={(e) => setSearchQuery(e.target.value)}
+                                                className={classes.searchInput}
+                                                disableUnderline={true}
+                                                InputProps={{
+                                                    startAdornment: 
+                                                        <InputAdornment position="start">
+                                                            <IconButton
+                                                            edge="end"
+                                                            >
+                                                                <SearchIcon fontSize="medium"/>
+                                                            </IconButton>
+                                                        </InputAdornment>,
+                                                    endAdornment: 
+                                                        <InputAdornment position="end" className="purpleButton">
+                                                            <Button
+                                                            edge="end"
+                                                            onClick={searchPhotos}
+                                                            >
+                                                                <span className="nibbuttonText">Search</span>
+                                                            </Button>
+                                                        </InputAdornment>,
+                                                    // classes:{notchedOutline:classes.searchInput},
+                                                    disableUnderline: true,
+                                                }}
+                                                placeholder="Find Something"
+                                            />
+                                        </FormControl>
+                                    </Grid>
+                                   
                                 </Grid>
 
                                 {/* container for select boxes */}
@@ -269,16 +319,6 @@ const Dashboard =()=>{
                                 </div>
                             </Grid>
                         </Grid>
-                        
-                          {/* <Grid container spacing={3}>
-                            <Grid item xs={12} md={6}>
-                                <Card>
-                                    <CardHeader title="Sales by category over four years" className={classes.cardTitle} />
-                                    <Divider />
-                                  
-                                </Card>
-                            </Grid>                           
-                        </Grid> */}
                     </div>
                 </>
             }
